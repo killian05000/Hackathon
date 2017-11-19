5ee14808f1aa65c39a9b0fab708a8bf0430a1575
L0L022 <L0L022@openmailbox.org>
2017-11-19 14:37:46 +0100

    super blender ihm

--- blender.py
+++ blender.py
@@ -5,6 +5,11 @@ import bpy
 import collections
 from abc import ABC, abstractmethod
 import csv
+import random
+
+for k in ["hack_width", "hack_height", "hack_strength", "hack_z", "hack_frame"]:
+    if not k in bpy.context.object.keys():
+        bpy.context.object[k] = 0
 
 Impact = collections.namedtuple('Impact', ['x', 'y', 'strength', 'when'])
 
@@ -23,37 +28,64 @@ class FakeImpactReader(ImpactReader):
         impacts = []
         max = 10
         for i in range(max):
-            impacts.append(Impact(i/max, i/max, i/max, i/max))
+            impacts.append(Impact(random.random(), random.random(), random.random(), random.random()))
         return impacts
 
 # End Impact file
 
 class BlenderImpactZone(ImpactZone):
-    pass
+    def __init__(self):
+        self.g = bpy.data.groups.new('hack')
+        self.width = bpy.context.object["hack_width"]
+        self.height = bpy.context.object["hack_height"]
+        self.strength = bpy.context.object["hack_strength"]
+        self.z = bpy.context.object["hack_z"]
+        self.frame = bpy.context.object["hack_frame"]
 
-# g = bpy.data.groups.new('supergroupe')
-# g.objects.link(bpy.data.objects['Cube'])
-#
-# lamp_object.location = (5.0, 5.0, 5.0)
+    def applyImpact(self, impact):
+        bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=2, size=impact.strength*self.strength, location=(impact.x*self.width, impact.y*self.height, 0))
+        o = bpy.context.scene.objects.active
+        self.g.objects.link(o)
+        f = impact.when*self.frame
+        o.keyframe_insert(data_path="location", frame=f, index=2)
+        o.location[2] = self.z
+        o.keyframe_insert(data_path="location", frame=f-1, index=2)
 
 def main(context):
-    #bpy.ops.object.effector_add()
-    for ob in context.scene.objects:
-        print(ob)
+    ir = FakeImpactReader()
+    iz = BlenderImpactZone()
+    impacts = ir.getImpacts()
+    for impact in impacts:
+        iz.applyImpact(impact)
 
 class SimpleOperator(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.simple_operator"
     bl_label = "Simple Object Operator"
 
+    width = bpy.props.FloatProperty(name="Width", default=bpy.context.object["hack_width"])
+    height = bpy.props.FloatProperty(name="Height", default=bpy.context.object["hack_height"])
+    strength = bpy.props.FloatProperty(name="Strength", default=bpy.context.object["hack_strength"])
+    z = bpy.props.FloatProperty(name="Z", default=bpy.context.object["hack_z"])
+    frame = bpy.props.FloatProperty(name="Frame", default=bpy.context.object["hack_frame"])
+
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
 
     def execute(self, context):
+        bpy.context.object["hack_width"] = self.width
+        bpy.context.object["hack_height"] = self.height
+        bpy.context.object["hack_strength"] = self.strength
+        bpy.context.object["hack_z"] = self.z
+        bpy.context.object["hack_frame"] = self.frame
+
         main(context)
         return {'FINISHED'}
 
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
 
 def register():
     bpy.utils.register_class(SimpleOperator)
@@ -62,9 +94,5 @@ def register():
 def unregister():
     bpy.utils.unregister_class(SimpleOperator)
 
-
 if __name__ == "__main__":
     register()
-
-    # test call
-    bpy.ops.object.simple_operator()
