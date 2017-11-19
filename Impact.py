af9115e4a35e5aaf5dc55f713d7e5515af8ac5a8
Killian Wolfger <killianwolfger@hotmail.fr>
2017-11-19 11:08:55 +0100

    Delete Impact.py

--- Impact.py
+++
@@ -1,9 +0,0 @@
-class Impact:
-
-    def __init__(self, _x, _y, _time, _power):
-        self.x = _x
-        self.y = _y
-        self.time = _time
-        self.power = _power
-
-# war = Impact(10,20,5,1)
